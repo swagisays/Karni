619df941a27e814948ad65c8e8b1341dc2bd3b4c
swagisays <swamishubham614@gmail.com>
2024-12-17 14:12:55 +0530

    fix: Update error code and initialize global context in MongoDB connection functions
    - Changed error code from 1002 to 1001 in the MongoDB client creation error handling.
    - Added initialization of the global context in multiple functions to ensure GlobalContext is set before use.
    Solved Issue #1

--- karni/global/global.go
+++ karni/global/global.go
@@ -16,37 +16,53 @@ var (
 	mu            sync.Mutex
 )
 
+func initGlobalContext() {
+	if GlobalContext == nil {
+		GlobalContext = &AppContext{}
+	}
+}
 func SetClient(client *mongo.Client) {
+	initGlobalContext()
 	mu.Lock()
 	defer mu.Unlock()
 	GlobalContext.client = client
 }
 
 func SetDB(db *mongo.Database) {
+	initGlobalContext()
+
 	mu.Lock()
 	defer mu.Unlock()
 	GlobalContext.db = db
 }
 
 func GetClient() *mongo.Client {
+	initGlobalContext()
+
 	mu.Lock()
 	defer mu.Unlock()
 	return GlobalContext.client
 }
 
 func GetDB() *mongo.Database {
+	initGlobalContext()
+
 	mu.Lock()
 	defer mu.Unlock()
 	return GlobalContext.db
 }
 
 func SetGlobalContext(ctx *AppContext) {
+	initGlobalContext()
+
 	mu.Lock()
 	defer mu.Unlock()
 	GlobalContext = ctx
 }
 
 func GetGlobalContext() *AppContext {
+	initGlobalContext()
+
 	mu.Lock()
 	defer mu.Unlock()
 	return GlobalContext
